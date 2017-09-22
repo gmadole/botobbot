e6ad20d08669945f78e97dbd5b972b3db7e0d59f
kayhide <kayhide@gmail.com>
2017-09-22 15:01:22 +0900

    create handler.{challenge,receive}.

--- handler.js
+++ handler.js
@@ -1,6 +1,32 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
+module.exports.challenge = (event, context, callback) => {
+  console.log(JSON.stringify(event));
+
+  const params = event['queryStringParameters'];
+  console.log(JSON.stringify(params));
+  if (params['hub.mode'] === 'subscribe' &&
+      params['hub.verify_token'] === 'echo_back_token' ) {
+    const response = {
+      statusCode: 200,
+      body: params['hub.challenge']
+    };
+    callback(null, response);
+  } else {
+    console.error('Failed validation.');
+    const response = {
+      statusCode: 403,
+      body: 'Failed validation.'
+    };
+    callback(null, response);
+  }
+};
+
+
+module.exports.receive = (event, context, callback) => {
+  console.log(event['body']);
+  const body = JSON.parse(event['body']);
+
   const response = {
     statusCode: 200,
     body: JSON.stringify({
