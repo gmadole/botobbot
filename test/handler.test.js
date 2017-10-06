4f6f93d42f1ec6498f634d9301f5c3bf7c148259
kayhide <kayhide@gmail.com>
2017-10-06 17:51:24 +0900

    update test for global before.

--- test/handler.test.js
+++ test/handler.test.js
@@ -7,138 +7,126 @@ var proxyquire = require('proxyquire').noCallThru();
 var helper = require('./test-helper');
 var Localstack = require('./localstack');
 
-describe('handler', () => {
-  let inited;
-  before((done) => {
-    helper.db.init().then(data => {
-      inited = true;
-    }).then(done, done);
-  })
 
-  it('setups', () => {
-    assert(inited)
+describe('#challenge', () => {
+  let event;
+  let handle;
+
+  beforeEach(() => {
+    const stub = {
+      'aws-sdk': {
+        DynamoDB: Localstack.DynamoDB,
+        '@global': true
+      }
+    };
+    const handler = proxyquire('../app/handler', stub);
+    event = {
+      'queryStringParameters': {
+        'hub.mode': 'subscribe',
+        'hub.verify_token': 'echo_back_token',
+        'hub.challenge': 'abcd1234'
+      }
+    };
+    handle = promisify(handler.challenge.bind(handler));
   });
 
-  describe('#challenge', () => {
-    let event;
-    let handle;
+  it('callbacks with success response', () => {
+    return handle(event, {}).then((res) => {
+      assert(res.statusCode === 200);
+      assert(res.body === 'abcd1234');
+    });
+  });
 
+  context('when hub.mode is not subscribe', () => {
+    it('callbacks with failed response', () => {
+      event['queryStringParameters']['hub.mode'] = 'bad_mode';
+      return handle(event, {}).then((res) => {
+        assert(res.statusCode === 403);
+      });
+    });
+  });
+
+  context('when hub.verify_token is not correct', () => {
+    it('callbacks with failed response', () => {
+      event['queryStringParameters']['hub.verify_token'] = 'bad_token';
+      return handle(event, {}).then((res) => {
+        assert(res.statusCode === 403);
+      });
+    });
+  });
+});
+
+
+describe('#receive', () => {
+  let event;
+  let handle;
+  let messenger;
+
+  beforeEach(() => {
+    messenger = {
+      send: sinon.stub()
+    }
+    const stub = {
+      'aws-sdk': {
+        DynamoDB: Localstack.DynamoDB,
+        '@global': true
+      },
+      './messenger': messenger
+    };
+    const handler = proxyquire('../app/handler', stub);
+    handle = promisify(handler.receive.bind(handler));
+  });
+
+  context('with text message', () => {
     beforeEach(() => {
-      const stub = {
-        'aws-sdk': {
-          DynamoDB: Localstack.DynamoDB,
-          '@global': true
-        }
-      };
-      const handler = proxyquire('../app/handler', stub);
       event = {
-        'queryStringParameters': {
-          'hub.mode': 'subscribe',
-          'hub.verify_token': 'echo_back_token',
-          'hub.challenge': 'abcd1234'
-        }
+        body: JSON.stringify(helper.fixture.read('receive_event'))
       };
-      handle = promisify(handler.challenge.bind(handler));
     });
 
     it('callbacks with success response', () => {
       return handle(event, {}).then((res) => {
         assert(res.statusCode === 200);
-        assert(res.body === 'abcd1234');
       });
     });
 
-    context('when hub.mode is not subscribe', () => {
-      it('callbacks with failed response', () => {
-        event['queryStringParameters']['hub.mode'] = 'bad_mode';
-        return handle(event, {}).then((res) => {
-          assert(res.statusCode === 403);
-        });
-      });
-    });
-
-    context('when hub.verify_token is not correct', () => {
-      it('callbacks with failed response', () => {
-        event['queryStringParameters']['hub.verify_token'] = 'bad_token';
-        return handle(event, {}).then((res) => {
-          assert(res.statusCode === 403);
-        });
+    it('calls messenger.send with given text', () => {
+      return handle(event, {}).then((res) => {
+        assert(messenger.send.calledOnce);
+        assert(messenger.send.getCall(0).args[0] === '6789012345678901');
+        assert(messenger.send.getCall(0).args[1].includes('Hello!'));
       });
     });
   });
 
-
-  describe('#receive', () => {
-    let event;
-    let handle;
-    let messenger;
-
+  context('with image message', () => {
     beforeEach(() => {
-      messenger = {
-        send: sinon.stub()
-      }
-      const stub = {
-        'aws-sdk': {
-          DynamoDB: Localstack.DynamoDB,
-          '@global': true
-        },
-        './messenger': messenger
+      event = {
+        body: JSON.stringify(helper.fixture.read('receive_event_image'))
       };
-      const handler = proxyquire('../app/handler', stub);
-      handle = promisify(handler.receive.bind(handler));
     });
 
-    context('with text message', () => {
-      beforeEach(() => {
-        event = {
-          body: JSON.stringify(helper.fixture.read('receive_event'))
-        };
-      });
-
-      it('callbacks with success response', () => {
-        return handle(event, {}).then((res) => {
-          assert(res.statusCode === 200);
-        });
-      });
-
-      it('calls messenger.send with given text', () => {
-        return handle(event, {}).then((res) => {
-          assert(messenger.send.calledOnce);
-          assert(messenger.send.getCall(0).args[0] === '6789012345678901');
-          assert(messenger.send.getCall(0).args[1].includes('Hello!'));
-        });
+    it('calls messenger.send twice with some text and with image url', () => {
+      return handle(event, {}).then(() => {
+        assert(messenger.send.calledTwice);
+        assert(messenger.send.getCall(0).args[0] === '6789012345678901');
+        assert(messenger.send.getCall(0).args[1].length > 0);
+        assert(messenger.send.getCall(1).args[0] === '6789012345678901');
+        assert(messenger.send.getCall(1).args[1].includes('https:\\/\\/botobbot.test\\/path\\/to\\/image.jpg?xxx=abcdef'));
       });
     });
 
-    context('with image message', () => {
-      beforeEach(() => {
-        event = {
-          body: JSON.stringify(helper.fixture.read('receive_event_image'))
-        };
-      });
-
-      it('calls messenger.send twice with some text and with image url', () => {
-        return handle(event, {}).then(() => {
-          assert(messenger.send.calledTwice);
-          assert(messenger.send.getCall(0).args[0] === '6789012345678901');
-          assert(messenger.send.getCall(0).args[1].length > 0);
-          assert(messenger.send.getCall(1).args[0] === '6789012345678901');
-          assert(messenger.send.getCall(1).args[1].includes('https:\\/\\/botobbot.test\\/path\\/to\\/image.jpg?xxx=abcdef'));
-        });
-      });
-
-      it('creates photo record', () => {
-        const db = new Localstack.DynamoDB.DocumentClient();
-        const params = { TableName: 'photos', Select: 'COUNT' };
-        let org;
-        return promisify(db.scan.bind(db))(params).then((data) => {
-          org = data.Count;
-          return handle(event, {});
-        }).then(() => {
-          return promisify(db.scan.bind(db))(params);
-        }).then((data) => {
-          assert(data.Count - org === 1);
-        });
+    it('creates photo record', () => {
+      const db = new Localstack.DynamoDB.DocumentClient();
+      const params = { TableName: 'photos', Select: 'COUNT' };
+      let org;
+      return promisify(db.scan.bind(db))(params).then((data) => {
+        org = data.Count;
+        return handle(event, {});
+      }).then(() => {
+        return promisify(db.scan.bind(db))(params);
+      }).then((data) => {
+        assert(data.Count - org === 1);
       });
     });
   });
--- test/test-helper.js
+++ test/test-helper.js
@@ -14,24 +14,32 @@ module.exports.fixture = {
   }
 }
 
-module.exports.db = {
-  init() {
-    const config = yaml.safeLoad(fs.readFileSync('serverless.yml', 'utf8'));
-    if (config.resources && config.resources.Resources) {
-      const resources = config.resources.Resources;
-      const tables = Object.keys(resources)
-            .filter(x => resources[x].Type == 'AWS::DynamoDB::Table');
-      const db = new Localstack.DynamoDB();
-      const createTable = promisify(db.createTable.bind(db));
-      const deleteTable = promisify(db.deleteTable.bind(db));
-      const ps = tables.map(x => {
-        const params = resources[x].Properties
-        deleteTable({TableName: params.TableName})
-          .catch(() => {})
-          .then(() => createTable(params))
-      })
-      return Promise.all(ps);
-    }
-    return Promise.resolve();
-  }
+function readResources() {
+  const config = yaml.safeLoad(fs.readFileSync('serverless.yml', 'utf8'));
+  return config.resources && config.resources.Resources;
 }
+
+function initDb() {
+  const resources = readResources();
+  if (resources) {
+    const tables = Object.keys(resources)
+          .filter(x => resources[x].Type == 'AWS::DynamoDB::Table');
+    const db = new Localstack.DynamoDB();
+    const createTable = promisify(db.createTable.bind(db));
+    const deleteTable = promisify(db.deleteTable.bind(db));
+    const ps = tables.map(x => {
+      const params = resources[x].Properties
+      deleteTable({TableName: params.TableName})
+        .catch(() => {})
+        .then(() => createTable(params))
+    })
+    return Promise.all(ps);
+  }
+  return Promise.resolve();
+}
+
+
+before((done) => {
+  initDb().then(data => {
+  }).then(done, done);
+})
